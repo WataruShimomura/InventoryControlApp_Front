7846cc718cf5e491aa48ec6ade5823efb8beebe7
Koji Sugimoto <kojisugimoto@AppirionoMacBook-Pro.local>
2019-10-23 21:46:30 +0900

    init

---
+++ store/sample/Sample.ts
@@ -0,0 +1,46 @@
+import { VuexModule, Module, Mutation, getModule, Action } from 'vuex-module-decorators'
+import store from "~/store/store";
+
+export interface ISampleState {
+  param1 :string;
+}
+
+@Module({dynamic: true, store, namespaced: true, name: 'sampleStore' })
+class Sample extends VuexModule implements ISampleState {
+  param1 = '';
+
+  @Mutation
+  addPost(param: string) {
+    this.param1 =param
+  }
+
+  @Action
+  async loadPosts() {
+    const posts = await fetchPosts()
+    posts.forEach(post => {
+      this.addPost(post.param1)
+    })
+  }
+}
+
+// この関数はあくまでも非同期にデータを取ってくるしくみ紹介
+const fetchPosts = (): Promise<ISampleState[]> => {
+  return new Promise(resolve => {
+    const dummys: ISampleState[] = [
+      {
+        param1: "1"
+      },
+      {
+        param1: "2"
+
+      },
+      {
+        param1: "3"
+      }
+    ]
+    resolve(dummys)
+  })
+}
+
+// 外部利用するためにgetModuleを使う
+export const sampleModule = getModule(Sample);
