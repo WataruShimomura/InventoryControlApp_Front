c7debc7be55e7356d16c397c9aed8174480c61fe
Koji Sugimoto <kojisugimoto@AppirionoMacBook-Pro.local>
2019-10-29 23:36:16 +0900

    add config bootstrap

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,8 @@ export default {
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     '@nuxtjs/pwa',
-    '@nuxtjs/proxy'
+    '@nuxtjs/proxy',
+    'bootstrap-vue/nuxt'
   ],
   /*
    ** Axios module configuration
